d18528f80822b69a18f9a51a649334a27a74744e
Evapavo <32174314+Evapavo@users.noreply.github.com>
2017-12-14 21:35:24 +0100

    1 iter done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,34 @@
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.direction = 'up';
+    //this.interval
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    setInterval (()=> this.update(),1000)
+  ;
+}
+
+
+
+  stop() {
+    console.log('hola');
+  }
+
+  update() {
+    this.log ();
+  }
+  _passengersEnter() {}
+  _passengersLeave() {}
+  floorUp() {}
+  floorDown() {}
+  call() {}
+  log() {
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,7 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+const elevator1 = new Elevator();
+const person1 = new Person();
+
+elevator1.update()
